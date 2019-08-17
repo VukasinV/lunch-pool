8b17f78ad6b6535fb31a8c59c721cceada40ed49
VukasinV <vranic.vukasin@gmail.com>
2019-08-18 01:21:39 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PollComponent } from './poll/poll.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
+import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PollComponent,
+    RestaurantComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule
